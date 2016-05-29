56c43499569efde2f2ba27044b78b3604864fe70
jv <mmm2>
2016-05-29 10:00:32 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+        ArrayList as=new ArrayList<String>(Arrays.asList("a","sd","as", "adf"));
+
+        System.out.println(as);
+    }
+}
